3dd6615d773d96e06fdc5f2593ab113eefa8e74c
Rupok <34137227+rupokswe@users.noreply.github.com>
2020-12-24 17:50:51 +0600

    Add files via upload

---
+++ Connections/job.php
@@ -0,0 +1,16 @@
+<?php
+# FileName="Connection_php_mysql.htm"
+# Type="MYSQL"
+# HTTP="true"
+$hostname_job = "sql303.epizy.com";
+$database_job='epiz_27519215_job';
+$username_job = "epiz_27519215";
+$password_job = "Tw8Tw1mTtqvS";
+$epiz_27519215_job;
+$database_job = mysqli_connect($hostname_job, $username_job, $password_job,"epiz_27519215_job") or trigger_error(mysqli_error(),E_USER_ERROR);*/
+
+
+
+
+
+?>
